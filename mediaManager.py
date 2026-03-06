32228d28116cd2e79b50c90b8bfd0f2c8cc4c469
romasenatosu <roma.senatosu@gmail.com>
2026-03-07 02:44:04 +0300

    image is not found error is handled

--- mediaManager.py
+++ mediaManager.py
@@ -13,19 +13,22 @@ class MediaManager:
         if file_id:
             try:
                 return await self.bot.send_photo(
-                    chat_id=chat_id,
-                    photo=file_id,
-                    caption=text
+                    chat_id=chat_id, photo=file_id, caption=text
                 )
             except TelegramError:
                 post["file_id"] = None
 
-        with open(image, "rb") as img:
-            msg = await self.bot.send_photo(
-                chat_id=chat_id,
-                photo=img,
-                caption=text
-            )
+        try:
+            with open(image, "rb") as img:
+                msg = await self.bot.send_photo(
+                    chat_id=chat_id, photo=img, caption=text
+                )
+
+        except FileNotFoundError:
+            print("[!] Image file is not found!")
+            print("[*] Sending post without photo...")
+
+            msg = await self.bot.send_message(chat_id=chat_id, text=text)
 
         post["file_id"] = msg.photo[-1].file_id
         return msg
